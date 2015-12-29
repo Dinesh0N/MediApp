174ea05d116fcfa228d558f237f2e83c7db3a5c8
Enrique Jeronimo <lejm.18@gmail.com>
2015-12-29 14:18:48 -0600

    ImagenesNuevas

--- app/src/main/java/com/qiqeapps/mediapp/RegistroNuevoConsulta.java
+++ app/src/main/java/com/qiqeapps/mediapp/RegistroNuevoConsulta.java
@@ -1,19 +1,15 @@
 package com.qiqeapps.mediapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.EditText;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 public class RegistroNuevoConsulta extends AppCompatActivity {
 
-    EditText txtFechaC;
+    EditText txtFechaHora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +21,11 @@ public class RegistroNuevoConsulta extends AppCompatActivity {
         long date = System.currentTimeMillis();
 
 
-        txtFechaC = (EditText) findViewById(R.id.txtConsultaFecha);
+        txtFechaHora = (EditText) findViewById(R.id.txtConsultaFechaHora);
 
         SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy, HH:mm a");
         String dateString = sdf.format(date);
-        txtFechaC.setText(dateString);
+        txtFechaHora.setText(dateString);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
